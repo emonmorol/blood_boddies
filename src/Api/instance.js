dbfe533eb14d082e1886ae7b2dd9107d78d96ff8
emonmorol <ekramulhasane69@gmail.com>
2022-05-21 02:17:05 +0600

    jwt token authorization implimented

--- src/Api/instance.js
+++ src/Api/instance.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const baseAxios = axios.create({
   baseURL: "http://localhost:5000",
+  headers: {
+    authorization: `Bearer ${localStorage.getItem("accessJwtToken")}`,
+  },
 });
 
 export default baseAxios;
