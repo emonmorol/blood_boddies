d4431923f949724c67ec7094bd0f5e39a48835d4
emonmorol <ekramulhasane69@gmail.com>
2022-05-21 13:06:44 +0600

    jwt token validation

--- src/Api/instance.js
+++ src/Api/instance.js
@@ -1,10 +1,95 @@
 import axios from "axios";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../firebase.init";
 
 const baseAxios = axios.create({
   baseURL: "http://localhost:5000",
-  headers: {
-    authorization: `Bearer ${localStorage.getItem("accessJwtToken")}`,
-  },
 });
 
+baseAxios.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    if (!config.headers.authorization) {
+      config.headers.authorization = `${localStorage.getItem(
+        "accessJwtToken"
+      )}`;
+    }
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    // signOut(auth);
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+baseAxios.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    // const navigate = useNavigate();
+    if (error.response.status === 403) {
+      // refresh token
+      // send to the server
+      signOut(auth);
+      // navigate("/login");
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default baseAxios;
+
+// import axios from "axios";
+// import { signOut } from "firebase/auth";
+// import { useNavigate } from "react-router-dom";
+// import auth from "../firebase.init";
+
+// const baseAxios = axios.create({
+//   baseURL: "http://localhost:5000",
+//   headers: {
+//     authorization: localStorage.getItem("accessJwtToken"),
+//   },
+// });
+
+// baseAxios.interceptors.request.use(
+//   function (config) {
+//     if (!config.headers.authorization) {
+//       config.headers.authorization = localStorage.getItem("accessJwtToken");
+//     }
+//     // return config;
+//   },
+//   function (error) {
+//     console.log(error);
+//     return Promise.reject(error);
+//   }
+// );
+
+// // Add a response interceptor
+// baseAxios.interceptors.response.use(
+//   function (response) {
+//     console.log(response);
+//     // Any status code that lie within the range of 2xx cause this function to trigger
+//     // Do something with response data
+//     return response;
+//   },
+//   function (error) {
+//     // Any status codes that falls outside the range of 2xx cause this function to trigger
+//     // Do something with response error
+
+//     const navigate = useNavigate();
+//     signOut(auth);
+//     navigate("/login");
+
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default baseAxios;
