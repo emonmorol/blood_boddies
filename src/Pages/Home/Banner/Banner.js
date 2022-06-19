57d2d74396ab991b33572f2145cae851bc9d1778
emonmorol <ekramulhasane69@gmail.com>
2022-06-19 17:31:17 +0600

    custom button created

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -21,10 +21,8 @@ const Banner = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <div className="bg-blood-banner bg-no-repeat px-3 bg-cover w-full min-h-screen flex justify-center items-center">
+          <div className="bg-blood-banner relative bg-no-repeat px-3 bg-cover w-full min-h-screen flex justify-center items-center z-0">
             <div className="banner-inner bg-white bg-opacity-90 lg:bg-opacity-80 lg:w-3/5">
-              {/* <div className=""></div>
-              <div className="absolute top-0 right-0 w-40 h-40 hover:w-full hover:h-full border-t-[6px] border-r-[6px] border-primary"></div> */}
               <div className="p-10 flex flex-col justify-center items-center">
                 <p className="text-center uppercase font-bold text-gray-600 text-xl tracking-widest">
                   Donate blood , save life !
@@ -32,19 +30,19 @@ const Banner = () => {
                 <p className="text-center uppercase font-bold text-gray-600 text-xl lg:text-3xl my-10 lg:leading-[20px]">
                   YOUR donation can bring smile at others face
                 </p>
-                <button className="btn btn-primary font-bold ">
-                  <span>Donate Now</span>
-                  <i className="fa-solid fa-arrow-right ml-2"></i>
+                <button className="button">
+                  <>
+                    <span>Donate Now</span>
+                    <i className="fa-solid fa-arrow-right ml-2"></i>
+                  </>
                 </button>
               </div>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="bg-blood-banner bg-no-repeat px-3 bg-cover w-full min-h-screen flex justify-center items-center">
+          <div className="bg-blood-banner relative z-0 bg-no-repeat px-3 bg-cover w-full min-h-screen flex justify-center items-center">
             <div className="banner-inner bg-white bg-opacity-90 lg:bg-opacity-80 lg:w-3/5">
-              {/* <div className="absolute bottom-0 left-0 w-40 h-40 hover:w-full hover:h-full border-b-[6px] border-l-[6px] border-primary"></div>
-              <div className="absolute top-0 right-0 w-40 h-40 hover:w-full hover:h-full border-t-[6px] border-r-[6px] border-primary"></div> */}
               <div className="p-10 flex flex-col justify-center items-center">
                 <p className="text-center  uppercase font-bold text-gray-600 text-xl tracking-widest">
                   Donate blood , save life !
@@ -52,7 +50,7 @@ const Banner = () => {
                 <p className="text-center uppercase font-bold text-gray-600 text-xl lg:text-3xl my-10 lg:leading-[20px]">
                   Donate Your Blood And Inspires to others
                 </p>
-                <button className="btn btn-primary font-bold ">
+                <button className="button">
                   <span>join with us</span>
                   <i className="fa-solid fa-arrow-right ml-2"></i>
                 </button>
